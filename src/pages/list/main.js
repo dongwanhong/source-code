c2311558d97cadf722307a9e6b62519ee3af0f01
dongwanhong <dongwhchn@163.com>
2019-11-14 22:42:22 +0800

    feat(src/*): 搭建案例展示页面的基本机构和样式

---
+++ src/pages/list/main.js
@@ -0,0 +1,8 @@
+import './index.less'
+import '../../styles/index.less'
+
+/* eslint-disable global-require */
+if (process.env.NODE_ENV !== 'production') {
+  require('./index.html')
+}
+/* eslint-enable global-require */
